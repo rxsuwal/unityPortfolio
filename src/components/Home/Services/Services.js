271d7f78b7de9f38907cae3b58939da74d7739d7
Kaal Vairab <itskaalvairan@gmail.com>
2021-05-17 12:46:01 +0545

    Hero services team completed,

--- src/components/Home/Services/Services.js
+++ src/components/Home/Services/Services.js
@@ -1,26 +1,44 @@
-import React from 'react'
-import styles from './Services.module.scss'
+import React, { Component } from 'react'
+import './Services.scss'
 
 import Slider from 'react-slick'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 
 import imgSlide from '../../../assets/img/mac.png'
+import { render } from '@testing-library/react'
 
-const Services = () => {
+class Services extends  Component {
 
+    constructor(props){
+        super(props);
+        this.state = {
+          nav1: null,
+          nav2: null
+        };
+      }
+    
+      componentDidMount(){
+        this.setState({
+          nav1: this.slider1,
+          nav2: this.slider2
+        });
+      }
    
+
+    
+ 
+   render(){
 
     const txtSliderSettings ={
         arrows: false,
-        // asNavFor:'imgSlide',
         dots: true,
         customPaging: function(i) {
             i++
             return (
                
               <a>
-                {'0' + i}
+                {i}
               </a>
             );
           },
@@ -29,71 +47,66 @@ const Services = () => {
     }
 
     const imgSliderSettings = {
-        arrows:true,
         dots:false,
-        // asNavFor:'txtSlide',
+        arrows:true,
+        nextArrow:<button><i class="fi-xtlrxl-arrow-simple-thin"></i></button>,
+        prevArrow:<button><i class="fi-xtllxl-arrow-simple-thin"></i></button>,
+
+        
     }
+
+
     return (
-        <div className={styles.services}>
-            <div className={styles.txt}>
+        <div className='services'>
+            <div className='servicesTxt'>
                 <h3>services</h3>
 
-                <Slider {...txtSliderSettings}>
-                    <div className={styles.txtSlide}>
+                <Slider {...txtSliderSettings} 
+                        className='txtSlider'
+                        asNavFor={this.state.nav2}
+                        ref={slider => (this.slider1 = slider)}>
+
+                    <div className="txtSlide">
                         <h1>We brings a wonderful artistic sense to project</h1>
                         <p>Nostrud consectetur commodo nostrud deserunt sint ad qui fugiat et commodo aliquip aute do elit. Irure dolor quis non veniam eiusmod veniam ex ea do. Magna in duis non deserunt aliqua et elit consectetur laboris.</p>
                     </div>
 
-                    <div className={styles.txtSlide}>
-                        <h1>We brings a wonderful artistic sense to project</h1>
-                        <p>Nostrud consectetur commodo nostrud deserunt sint ad qui fugiat et commodo aliquip aute do elit. Irure dolor quis non veniam eiusmod veniam ex ea do. Magna in duis non deserunt aliqua et elit consectetur laboris.</p>
-                    </div>
-                    <div className={styles.txtSlide}>
+                    <div className="txtSlide">
                         <h1>We brings a wonderful artistic sense to project</h1>
                         <p>Nostrud consectetur commodo nostrud deserunt sint ad qui fugiat et commodo aliquip aute do elit. Irure dolor quis non veniam eiusmod veniam ex ea do. Magna in duis non deserunt aliqua et elit consectetur laboris.</p>
                     </div>
 
-         
 
                 </Slider>
 
             </div>
 
-            <div className={styles.img}>
+            <div className='servicesImg'>
 
-                <Slider {...imgSliderSettings} className={styles.imgSlider}>
-                    <div className={styles.imgSlide}>
-
-                        <img src={imgSlide} alt=""/>
-                        
-
-                    </div>
-
-                    <div className={styles.imgSlide}>
-
-                        <img src={imgSlide} alt=""/>
-                        
-
-                    </div>
-                    <div className={styles.imgSlide}>
-
-                        <img src={imgSlide} alt=""/>
-                        
-
-                    </div>
-                    <div className={styles.imgSlide}>
+                <Slider {...imgSliderSettings} 
+                        className="imgSlider"    
+                        asNavFor={this.state.nav1}
+                        ref={slider => (this.slider2 = slider)}>
+                    <div className="imgSlide">
 
                         <img src={imgSlide} alt=""/>
                         
 
                     </div>
 
+                    <div className="imgSlide">
+
+<img src={imgSlide} alt=""/>
+
+
+</div>
                 </Slider>
 
             </div>
             
         </div>
     )
+   }
 }
 
 export default Services
--- src/components/Home/Team/Team.js
+++ src/components/Home/Team/Team.js
@@ -14,13 +14,18 @@ import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
 const Team = () => {
 
     const sliderSetting ={
-      
       infinite: true,
       speed: 500,
       slidesToShow: 1,
       slidesToScroll: 1,
-      vertical: true,
+      vertical: false,
       verticalSwiping: true,
+      beforeChange: function(currentSlide, nextSlide) {
+        console.log("before change", currentSlide, nextSlide);
+      },
+      afterChange: function(currentSlide) {
+        console.log("after change", currentSlide);
+      },
       arrow:true,
       prevArrow: <button><ArrowUpwardIcon style={{fontSize:"3rem"}}/></button>,
       nextArrow:<button><ArrowDownwardIcon style={{fontSize:"3rem"}}/></button>,
@@ -40,8 +45,9 @@ const Team = () => {
         <div className="team">
 
             <Slider {...sliderSetting} className="slider">
+
                 <div className="slide">
-                    <h1>Designer & Coder</h1>
+                    <h1>Designer & Coder Designer & Coder</h1>
                     <p>Esse Reprehenderit consequat elit quis quis qui excepteur aliqua est id do voluptate voluptate proident dolor. reprehenderit mollit deserunt aute voluptate aliquip elit ex incididunt veniam sunt incididunt.</p>
                 </div>
 
@@ -53,10 +59,13 @@ const Team = () => {
                 <div className="slide">
                     <h1>Designer & Coder</h1>
                     <p>Esse Reprehenderit consequat elit quis quis qui excepteur aliqua est id do voluptate voluptate proident dolor. reprehenderit mollit deserunt aute voluptate aliquip elit ex incididunt veniam sunt incididunt.</p>
-                </div> <div className="slide">
+                </div> 
+
+                <div className="slide">
                     <h1>Designer & Coder</h1>
                     <p>Esse Reprehenderit consequat elit quis quis qui excepteur aliqua est id do voluptate voluptate proident dolor. reprehenderit mollit deserunt aute voluptate aliquip elit ex incididunt veniam sunt incididunt.</p>
                 </div>
+                
             </Slider>
             
         </div>
